adc86c57fe461a8189eec0b3c03ae31874275f25
julio cesar alves <jdetecnologiaas@gmail.com>
2023-01-31 21:12:37 -0300

    adicionado o clean dados

--- src/helper/loginRedirect.js
+++ src/helper/loginRedirect.js
@@ -1,4 +1,7 @@
+import { cleanDadosLogin } from "./dadosLoginCheck"
+
 export function loginRedirect(){
     alert("Favor autenticar novamente no sistema, você será redirecionado para a página de login!")
+    cleanDadosLogin();
     location.href = "/autheticacion/sign-in"
 }
