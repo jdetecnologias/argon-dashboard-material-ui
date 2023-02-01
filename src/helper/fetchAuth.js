4b97d90bb961c99bb361465284efc86189198b2f
julio cesar alves <jdetecnologiaas@gmail.com>
2023-01-31 21:11:02 -0300

    adiconando a chamada a funcao loginRedirect na autenticacao

--- src/helper/fetchAuth.js
+++ src/helper/fetchAuth.js
@@ -1,10 +1,11 @@
+import { loginRedirect } from "./loginRedirect";
+
 export function fetchAuth(url, config){
     return new Promise((resolve, reject)=>{
         fetch(url, config).then(result=>{
             console.log(result)
             if(result.status ===  401 ){
-                alert("autenticação expirada, o site será redirecionado para página de login!")
-                location.href = "/authentication/sign-in";
+                loginRedirect();
             }else{
                 resolve(result);
             }
