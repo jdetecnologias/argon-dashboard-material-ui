9e3f51b91037ecccda64b051c08b50c389a033a9
julio cesar alves <jdetecnologiaas@gmail.com>
2023-02-02 23:34:42 -0300

    aciionado a validacao de data utilizando a biblioteca dayjs

--- src/layouts/authentication/sign-up/controller/cadastrarUsuarioController.js
+++ src/layouts/authentication/sign-up/controller/cadastrarUsuarioController.js
@@ -1,10 +1,11 @@
+import dayjs from "dayjs";
 import { isValidPasswordLenght } from "helper/validate";
-import { isValidEmail, isValidDate, isValidGenero } from "helper/validate";
+import { isValidEmail,  isValidGenero } from "helper/validate";
 import { cadastrarUsuarioModel } from "../model/cadastrarUsuarioModel";
 
 export async function cadastrarUsuario(dadosUsuario, setStatusCadastro){
     const isvalidEmail = isValidEmail(dadosUsuario.email);
-    const isvalidDate = isValidDate(dadosUsuario.dt_nasc);
+    const isvalidDate = dayjs(dadosUsuario.dt_nasc).isValid();
     const isvalidGenero = isValidGenero(dadosUsuario.genero);
     const isvalidPassword = isValidPasswordLenght(dadosUsuario.senha);
     const messageErrorList = [];
