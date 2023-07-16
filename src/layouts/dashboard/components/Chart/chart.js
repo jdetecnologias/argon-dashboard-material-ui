1f8af41c2c725d4a20b165f3dfd50889914f075d
julio cesar alves <jdetecnologiaas@gmail.com>
2023-07-16 10:57:00 -0300

    add light theme Handler  to graphics

--- src/layouts/dashboard/components/Chart/chart.js
+++ src/layouts/dashboard/components/Chart/chart.js
@@ -2,17 +2,21 @@ import GradientLineChart from "examples/Charts/LineCharts/DefaultLineChart";
 import { getAverage } from "helper/math";
 import PropTypes from "prop-types";
 export default function Chart(props){
-    const {dataChart, _className,title} = props;    
+    const {dataChart, _className,title, lightTheme} = props;   
+    
+    const bgClassName = lightTheme ? "" : "bg-black";
+    let mainClassName ="grid grid-cols-10 "+bgClassName; 
+    mainClassName +=  _className !== undefined ? " "+_className : ""; 
     return (
-        <div className={_className+" grid grid-cols-10 bg-black"}>
-            <div className="col-span-8 invert">
+        <div className={mainClassName}>
+            <div className={lightTheme?"col-span-8":"col-span-8 invert"}>
                 <GradientLineChart
                 title={title}
                 chart={dataChart}
                 height={200}
                 />
             </div>
-            <div className="text-lime-300 col-span-2">
+            <div className={lightTheme?"col-span-2":"text-lime-300 col-span-2"}>
                 <div className="text-sm">
                     Média do período:
                 </div>
@@ -29,4 +33,5 @@ Chart.propTypes = {
     dataChart: PropTypes.objectOf(PropTypes.array).isRequired,
     _className: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    lightTheme: PropTypes.bool.isRequired,
 }
--- src/layouts/dashboard/components/Chart/chartList.js
+++ src/layouts/dashboard/components/Chart/chartList.js
@@ -1,29 +1,34 @@
 import Map from "layouts/dashboard/map/map";
 import Chart from "./chart"
 import PropTypes from "prop-types";
+import ChartThemeSelector from "./chartThemeSelector";
+import { useState } from "react";
 export default function ChartList(props){
-    const {dataChart} = props;    
+    const {dataChart} = props;   
+    const [lightTheme, setLightTheme] = useState(false); 
     return (
-        <div className=" md:grid md:grid-cols-4 flex flex-col-reverse mt-16">
-            <div className="grid grid-cols-1 gap-1 col-span-3">
-            {
-                dataChart.datasets && dataChart.datasets.map((dataset, key)=>{
-                return <
-                           
-                            Chart 
-                            key={key} 
-                            title={dataset.label} 
-                            dataChart={{labels:dataChart.labels[key], datasets:[dataset]}}
-                        />
-                })
-            }
-            
+            <div className=" md:grid md:grid-cols-4 flex flex-col-reverse mt-16">
+                <div className="col-span-3">
+                    <ChartThemeSelector onChange={setLightTheme} value={lightTheme}/>
+                {
+                    dataChart.datasets && dataChart.datasets.map((dataset, key)=>{
+                    return <
+                            
+                                Chart 
+                                key={key} 
+                                title={dataset.label} 
+                                lightTheme={lightTheme}
+                                dataChart={{labels:dataChart.labels[key], datasets:[dataset]}}
+                            />
+                    })
+                }
+                
 
+                </div>
+                <div>
+                    <Map/>
+                </div>
             </div>
-            <div>
-                <Map/>
-            </div>
-        </div>
     )
 }
 
