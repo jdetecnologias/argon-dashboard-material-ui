eea70cf3864174f677db0a3352eb0239511d168e
julio cesar alves <jdetecnologiaas@gmail.com>
2023-08-10 23:50:32 -0300

    alteracoes para a plataforma verde aceessar os dados do paciente,

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -71,12 +71,22 @@ function DashboardNavbar({ absolute, light, isMini }) {
   const route = useLocation().pathname.split("/").slice(1);
 
   useEffect(()=>{
+    const queryString = window.location.search;
     
-    if(hasValidDadosLogin()){
-      let dadosLogin = getCookie("dadosLogin")
-      dadosLogin = JSON.parse(dadosLogin);
+    const urlParams = new URLSearchParams(queryString);
+    const adminAccess = urlParams.get('adminAccess') !== null && urlParams.get('adminAccess') === 'true' ;
+  
+    if(adminAccess){
+      const email = urlParams.get('email');
+      setEmail(email);
+    }else{
+      
+      if(hasValidDadosLogin()){
+        let dadosLogin = getCookie("dadosLogin")
+        dadosLogin = JSON.parse(dadosLogin);
 
-      setEmail(dadosLogin.email);
+        setEmail(dadosLogin.email);
+      }
     }
   },[])
 
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -33,7 +33,7 @@ import typography from "assets/theme/base/typography";
 // Data
 import gradientLineChartData from "layouts/dashboard/data/gradientLineChartData";
 
-import { getFitered } from "./controller/getFilteredController";
+import { getFitered, getFiteredByAdmin } from "./controller/getFilteredController";
 import { getCookie } from "helper/cookies";
 import { adapterGlycemia } from "./adapter/dataAdapter";
 import ArgonInput from "components/ArgonInput";
@@ -71,8 +71,18 @@ function Default() {
                                           ])
                                           
   useEffect(() => {
-    const dadoslogin = getDadosLogin();
-    filtrar(dadoslogin.email, data_inicio_filtro, data_fim_filtro,dadoslogin.token);
+    const queryString = window.location.search;
+    
+    const urlParams = new URLSearchParams(queryString);
+    const adminAccess = urlParams.get('adminAccess') !== null && urlParams.get('adminAccess') === 'true' ;
+
+    if(adminAccess){
+      const email = urlParams.get('email');
+      filtrarAdmin(email, data_inicio_filtro, data_fim_filtro);
+    }else{
+      const dadoslogin = getDadosLogin();
+      filtrar(dadoslogin.email, data_inicio_filtro, data_fim_filtro,dadoslogin.token);
+    }
   },[])
 
   useEffect(() => {
@@ -86,6 +96,18 @@ function Default() {
 
 function filtrar(email, data_inicio_filtro, data_fim_filtro, token){
   getFitered({email, data_inicio_filtro, data_fim_filtro, hora_inicio, hora_fim },token,(dados, messageErrorsList)=>{
+     
+    if(messageErrorsList.length > 0){
+      setMessageErrorList(messageErrorsList);
+    }else{
+      setListGlycemia(dados);
+    }
+  })
+}
+
+
+function filtrarAdmin(email, data_inicio_filtro, data_fim_filtro){
+  getFiteredByAdmin({email, data_inicio_filtro, data_fim_filtro, hora_inicio, hora_fim },(dados, messageErrorsList)=>{
      
     if(messageErrorsList.length > 0){
       setMessageErrorList(messageErrorsList);
@@ -124,9 +146,19 @@ function getValues(labelName){
 
 
 function handleFiltrar(){
-  const  dadosLogin = getDadosLogin();
+  const queryString = window.location.search;
+    
+  const urlParams = new URLSearchParams(queryString);
+  const adminAccess = urlParams.get('adminAccess') !== null && urlParams.get('adminAccess') === 'true' ;
 
-  filtrar(dadosLogin.email, data_inicio_filtro, data_fim_filtro, dadosLogin.token);
+  if(adminAccess){
+    const email = urlParams.get('email');
+    filtrarAdmin(email, data_inicio_filtro, data_fim_filtro);
+  }else{
+    const  dadosLogin = getDadosLogin();
+
+    filtrar(dadosLogin.email, data_inicio_filtro, data_fim_filtro, dadosLogin.token);
+  }
 }
 
 function getDadosLogin(){
