97116023b1f59e74c5320fe7ee58f2e54b8d1094
julio cesar alves <jdetecnologiaas@gmail.com>
2024-04-11 21:10:25 -0300

    colocando todos os icones do lado direito da tela da mesma cor

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -26,6 +26,7 @@ import ChartList from "./components/Chart/chartList";
 import fundo2 from "./assets/fundo2.png"
 import { appDataState, metaDataListState } from "../../stateHandler/atoms/atoms";
 import { GetLastAppData } from "./model/getAppDataModel";
+import { Fundo1 } from "./assets/fundo1";
 
 let qtyMapShow = -1;
 function Default() {
@@ -229,8 +230,8 @@ function getDadosLogin(){
         handleFiltrar={handleFiltrar}
       />
       <If test={messageErrorsList.length === 0} Else={<ErrorAlert messageErrorList={messageErrorsList} resetMessages={()=>setMessageErrorList([])}/>}>
-      <div className=" md:grid md:grid-cols-4 flex flex-col-reverse mt-16">
-        <div className="col-span-3">
+      <div className=" md:grid md:grid-cols-12 flex flex-col-reverse mt-16">
+        <div className="col-span-8">
           <ChartThemeSelector onChange={setLightTheme} value={lightTheme}/>
           <button className="btn btn-primary ml-2" onClick={()=>PrintContentCanvas("#grafico","#grafico canvas")}><Print collorFill="#fff" /></button>
             <div>
@@ -248,70 +249,69 @@ function getDadosLogin(){
               </div>
             </div>
         </div>
-        <div>
- 
+        <div className="col-span-4"> 
           <div>
               <h5 className="text-center text-sm font-extrabold">Horas de sono:</h5>
               <div className="grid grid-cols-2">
                   <div className="w-48">
-                      <Sleep collorFill="#000" _className=""/>
+                      <Sleep colorFill="lightblue" _className=""/>
                   </div>
                   <div className='w-48 font-bold'  style={{position:"relative"}}>
-                      <img src={fundo}/> 
+                      <Fundo1 colorFill="lightblue"/>
                       <span style={{position:"absolute", top:"50%", left:"50%", transform:"translateY(-50%) translateX(-50%)"}}>{appData.sleepTime?minutoToHour(appData.sleepTime):0}</span>
                   </div>
               </div> 
           </div>
-          <div>
-            <div className="grid grid-cols-3">
-                <div className="grid grid-rows-3">
-                    <h5 className="text-center text-sm font-extrabold">Passos Ativo</h5>
-                    <div className='w-32' style={{position:"relative"}}>
-                        <div style={{position:"absolute", left:"50%", transform:"translateX(-50%)"}}>
-                            <Steps collorFill="#000" _className="w-12"/>
-                        </div>
-                    </div>
-                    <div className="p-2 text-center text-xs font-extrabold">
-                        {appData.steps?parseInt(appData.steps):0}
-                    </div>
-                </div>
-                <div className="grid grid-rows-3">
-                    <h5 className="text-center text-sm font-extrabold">Atividades</h5>
-                    <div className='w-32'  style={{position:"relative"}}>
-                        <div style={{position:"absolute", left:"50%", transform:"translateX(-50%)"}}>
-                            <FrequencyHeart collorFill="#000" _className="w-12"/>
-                        </div>
-                    </div>
-                    <div className="p-2 text-center text-xs font-extrabold">
-                    {appData.activities?parseInt(appData.activities):0}
-                    </div>
-                </div>
-                <div className="grid grid-rows-3">
-                    <h5 className="text-center text-sm font-extrabold">Movimentos intensidade</h5>
-                    <div className='w-32'  style={{position:"relative"}}>
-                        <div style={{position:"absolute", left:"50%", transform:"translateX(-50%)"}}>
-                            <Activities  collorFill="#000" _className="w-12"/>
-                        </div>
-                    </div>
-                    <div className="p-2 text-center text-xs font-extrabold">
-                        {appData.intensityMovements?parseInt(appData.intensityMovements):0}
-                    </div>
-                </div>
-            </div> 
-          </div>
+
+          <div className="grid grid-cols-3">
+              <div className="grid grid-rows-3">
+                  <h5 className="text-center text-sm font-extrabold">Passos Ativo</h5>
+                  <div className='w-32' style={{position:"relative"}}>
+                      <div style={{position:"absolute", left:"50%", transform:"translateX(-50%)"}}>
+                          <Steps colorFill="lightblue" _className="w-12"/>
+                      </div>
+                  </div>
+                  <div className="p-2 text-center text-xs font-extrabold">
+                      {appData.steps?parseInt(appData.steps):0}
+                  </div>
+              </div>
+              <div className="grid grid-rows-3">
+                  <h5 className="text-center text-sm font-extrabold">Atividades</h5>
+                  <div className='w-32'  style={{position:"relative"}}>
+                      <div style={{position:"absolute", left:"50%", transform:"translateX(-50%)"}}>
+                          <FrequencyHeart colorFill="lightblue" _className="w-12"/>
+                      </div>
+                  </div>
+                  <div className="p-2 text-center text-xs font-extrabold">
+                  {appData.activities?parseInt(appData.activities):0}
+                  </div>
+              </div>
+              <div className="grid grid-rows-3">
+                  <h5 className="text-center text-sm font-extrabold">Movimentos intensidade</h5>
+                  <div className='w-32'  style={{position:"relative"}}>
+                      <div style={{position:"absolute", left:"50%", transform:"translateX(-50%)"}}>
+                          <Activities  colorFill="lightblue" _className="w-12"/>
+                      </div>
+                  </div>
+                  <div className="p-2 text-center text-xs font-extrabold">
+                      {appData.intensityMovements?parseInt(appData.intensityMovements):0}
+                  </div>
+              </div>
+          </div> 
+          
           <div>
             <div className="grid grid-cols-2">
             <div>
                     <h5 className="text-center text-sm font-extrabold">Distância</h5>
                     <div className='w-48 font-bold' style={{position:"relative"}}>
-                        <img src={fundo}/> 
+                    <Fundo1 colorFill="lightblue"/>
                         <div style={{position:"absolute", top:"50%", left:"50%", transform:"translateY(-50%) translateX(-50%)"}}>{appData.distance?parseInt(appData.distance):0}Mt</div>
                     </div>
                 </div>
                 <div>
                     <h5 className="text-center text-sm font-extrabold">Tempo Ativo</h5>
                     <div className='w-48 font-bold' style={{position:"relative"}}>
-                        <img src={fundo}/> 
+                        <Fundo1 colorFill="lightblue"/>
                         <div style={{position:"absolute", top:"50%", left:"50%", transform:"translateY(-50%) translateX(-50%)"}}>{appData.avtiveTime?minutoToHour(appData.avtiveTime):0}</div>
                     </div>
                 </div>
